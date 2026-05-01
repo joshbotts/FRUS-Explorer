37880af12e2041b1a423e177b624209538f4e1e0
Josh <jbotts@gmail.com>
2026-04-30 22:29:38 -0400

    Initial Commit

---
+++ FRUS Explorer/FRUS_ExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FRUS_ExplorerApp.swift
+//  FRUS Explorer
+//
+//  Created by Josh Botts on 4/30/26.
+//
+
+import SwiftUI
+
+@main
+struct FRUS_ExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
